f0a8c4094c96fca6b6f805c731e643adeaca191f
Maria Sokolova <maria.sokolova@jetbrains.com>
2024-12-20 11:36:31 +0100

    [stdlib] Moved common Atomics and AtomicArray tests to the test.concurrent.atomics package.

--- libraries/stdlib/test/concurrent/atomics/AtomicCommonTest.kt
+++ libraries/stdlib/test/concurrent/atomics/AtomicCommonTest.kt
@@ -3,7 +3,7 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 @file:OptIn(ExperimentalAtomicApi::class)
-package test.concurrent
+package test.concurrent.atomics
 
 import kotlin.concurrent.atomics.*
 import kotlin.test.*
